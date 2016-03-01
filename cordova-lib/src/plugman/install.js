aaa69f9012d82176a0c25c1b475b53765d3c3409
Brett Rudd <goya@apache.org>
2016-02-29 16:31:30 -0800

    [pgb] show sha of git plugins

--- cordova-lib/src/plugman/install.js
+++ cordova-lib/src/plugman/install.js
@@ -302,7 +302,7 @@ function runInstall(actions, platform, project_dir, plugin_dir, plugins_dir, opt
         }
         return Q();
     }
-    events.emit('log', 'Installing "' + pluginInfo.id + '@' + pluginInfo.version + '" for ' + platform);
+    events.emit('log', 'Installing "' + pluginInfo.id + '" at "' + pluginInfo.version + '" for ' + platform);
 
 
     var theEngines = getEngines(pluginInfo, platform, project_dir, plugin_dir);
